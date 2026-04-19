22e108f63522a551fd45e9eec8187dbe3427516e
brandoriginuk11-ops <brandoriginuk11@gmail.com>
2026-04-20 00:55:59 +0900

    Update Error.tsx

--- src/components/Common/Error/Error.tsx
+++ src/components/Common/Error/Error.tsx
@@ -2,28 +2,36 @@ import React from "react";
 import "./Error.scss";
 
 interface ErrorProps {
-  error: Error;
+  error: any;
 }
 
 export const Error = ({ error }: ErrorProps) => {
+  const message =
+    error?.response?.data?.message ||
+    error?.message ||
+    "Something went wrong while loading weather data.";
+
   return (
-    <>
-      <div className="error-container">
-        <img
-          src={require("../../../resources/error.png")}
-          className="error-image"
-          alt=""
-        />
-        <div className="error-title">Error</div>
-        <div className="error-message">{error.message}</div>
-      </div>
-    </>
+    <div className="error-container">
+      <img
+        src={require("../../../resources/error.png")}
+        className="error-image"
+        alt="Error"
+      />
+      <div className="error-title">Error</div>
+      <div className="error-message">{message}</div>
+    </div>
   );
 };
 
 export const ErrorHandler = (
-  error: Error,
+  error: any,
   info: { componentStack: string }
 ) => {
-  console.error(error);
+  console.error("Weather App Error:", {
+    message: error?.message,
+    response: error?.response?.data,
+    stack: error?.stack,
+    componentStack: info?.componentStack,
+  });
 };
